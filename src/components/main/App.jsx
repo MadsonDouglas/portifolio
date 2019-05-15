d594d1f864d0fa6408e2a71c90ea31a8fe03ca1a
MadsonDouglas <santos.madson94@gmail.com>
2019-05-15 19:01:14 -0300

    Criando o  navbar da aplicação

---
+++ src/components/main/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './App.css'
+
+import Nav from '../template/Nav'
+
+export default props =>
+        <div className="app">
+                <Nav />
+        </div>
---
+++ src/components/template/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Nav.css'
+
+export default props =>
+        <div className="menu">
+            <div className="logo">
+                <a href="/">
+                    <p><span>D</span>EV  <span>F</span>ullStack</p>
+                </a>
+            </div>
+            <div className="navegacao">
+                <a href="/">Home</a>
+                <a href="/">Sobre</a>
+                <a href="/">Trabalhos</a>
+                <a href="/">Blog</a>
+                <a href="/">Portifolio</a>
+            </div>
+        </div>
