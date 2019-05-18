7209a2d1699b4c1988c4337749128b29bedc10ab
MadsonDouglas <santos.madson94@gmail.com>
2019-05-18 14:43:07 -0300

    finalizando o componente profile do github

--- src/components/trabalhos/github/GitProfile.jsx
+++ src/components/trabalhos/github/GitProfile.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-
 import './GitProfile.css';
 
 const Profile = ({ user }) =>
     <div className="container-card">
         <div className="card">
-            <img src={user} alt="Avatar" />
+            <img src={user.avatar_url} alt="Avatar" />
             <div className="container">
-                <h4>Repositorios: <span>{user.public_repos}</span></h4>
+                <p>Repositorios: <span className="bg-g">{user.public_repos}</span></p>
             </div>
             <div className="container">
-                <h4>Seguidores: <span className="bg-b">{user.followers}</span></h4>
+                <p>Seguidores: <span className="bg-b">{user.followers}</span></p>
             </div>
             <div className="container">
-                <h4>Seguindo: <span className="bg-b">{user.following}</span></h4>
+                <p>Seguindo: <span className="bg-b">{user.following}</span></p>
             </div>
-            <div className="container">
-                <a href="">Ver perfil</a>
+            <div className="container link">
+                    <a href={user.html_url} target="_blank" rel="noopener noreferrer" className="bg-g">Ver perfil</a>
             </div>
-
-
         </div>
     </div>
 export default Profile;
